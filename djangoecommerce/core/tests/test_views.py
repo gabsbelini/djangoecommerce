73aa78b47110e01c704182dec779ca9388ea799f
gabsbelini <gabs_belini@hotmail.com>
2017-03-09 18:56:32 -0300

    Testes basicos adicionados, aula 10 finished

---
+++ djangoecommerce/core/tests/test_views.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from django.test import TestCase, Client
+
+
+class IndexViewTestCase(TestCase):
+
+    def setUP(self):
+        self.client = Client()
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        reponse = self.client.get('/')
+        self.assertEquals(reponse.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
