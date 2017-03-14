0711af4b96f92b59d050f490634eb37fdb2c0e91
gabsbelini <gabs_belini@hotmail.com>
2017-03-14 19:16:04 -0300

    catalog e models_catalog criados

--- djangoecommerce/djangoecommerce/settings.py
+++ djangoecommerce/djangoecommerce/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'core',
+    'catalog',
+
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
