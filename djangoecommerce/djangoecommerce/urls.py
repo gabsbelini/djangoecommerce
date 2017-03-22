f7605fcf82fa78a2bcf0f3076cbb7ed0fc08134f
gabsbelini <gabs_belini@hotmail.com>
2017-03-22 19:43:43 -0300

    aula 41 concluida

--- djangoecommerce/djangoecommerce/urls.py
+++ djangoecommerce/djangoecommerce/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.auth.views import login, logout
 
 from core import views
 from catalog import views as views_catalog
@@ -22,6 +24,9 @@ from catalog import views as views_catalog
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contato/$', views.contact, name='contact'),
+    url(r'^entrar/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^sair/$', logout, {'next_page': 'index'}, name='logout'),
+    url(r'^registro/$', views.register, name='register'),
     url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
     url(r'^admin/', admin.site.urls),
 ]
