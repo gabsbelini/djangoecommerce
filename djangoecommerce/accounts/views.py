065615bf90e5af7a0625d38ff3c4f68218ef166c
gabsbelini <gabs_belini@hotmail.com>
2017-04-03 17:01:39 -0300

    aula 48 concluida

--- djangoecommerce/accounts/views.py
+++ djangoecommerce/accounts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import User
 from .forms import UserAdminCreationForm
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+
+    template_name = 'accounts/index.html'
+
 
 
 class RegisterView(CreateView):
@@ -15,3 +22,4 @@ class RegisterView(CreateView):
 
 
 register = RegisterView.as_view()
+index = IndexView.as_view()
