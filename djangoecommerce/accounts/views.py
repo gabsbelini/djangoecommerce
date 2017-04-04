9c952ba09c7d9516a4b2c2de6165014513a3612e
gabsbelini <gabs_belini@hotmail.com>
2017-04-04 18:26:55 -0300

    aula 51 done.

--- djangoecommerce/accounts/views.py
+++ djangoecommerce/accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.views.generic import (
+    CreateView, TemplateView, UpdateView, FormView
+)
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import PasswordChangeForm
 
 from .models import User
 from .forms import UserAdminCreationForm
@@ -32,6 +35,23 @@ class UpdateUserView(LoginRequiredMixin, UpdateView):
         return self.request.user
 
 
+class UpdatePasswordView(LoginRequiredMixin, FormView):
+
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('accounts:index')
+    form_class = PasswordChangeForm
+
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(UpdatePasswordView, self).form_valid(form)
+
+
 register = RegisterView.as_view()
 index = IndexView.as_view()
 update_user = UpdateUserView.as_view()
+update_password = UpdatePasswordView.as_view()
