53138ccc614322411e154239b9ef21159790684a
gabsbelini <gabs_belini@hotmail.com>
2017-04-14 15:06:17 -0300

    aula 63 done.

--- djangoecommerce/checkout/tests/test_models.py
+++ djangoecommerce/checkout/tests/test_models.py
@@ -4,12 +4,24 @@ from model_mommy import mommy
 
 from checkout.models import CartItem
 
+
 class CartItemTestCase(TestCase):
 
     def setUp(self):
-        mommy.make(CartItem, _quantity=3)  # _quantity eh a quantidade de itens do modelo a serem gerados, nao eh a propriedade quantity do modelo.
+        """Cria 3 objetos CartItem.
+
+        A variavel _quantity eh a quantidade de objetos criados. NAO tem a Ver
+        com o atributo quantity do modelo.
+        """
+        mommy.make(CartItem, _quantity=3)
 
     def test_post_save_cart_item(self):
+        """Testa se quantity = 0 remove o item do carrinho.
+
+        Seleciona o primeiro cart_item, criado pelo setup (3 no total),
+        seta sua quantidade pra 0,deve ser apagado devido ao signal do models.
+        Verifica se a quantidade restante de cart_item eh 2 (3 - 1 = 2).
+        """
         cart_item = CartItem.objects.all()[0]
         cart_item.quantity = 0
         cart_item.save()
